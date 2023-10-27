f62e70bc7e4690096d819178470cbb221048c258
Pizsx69 <60088795+Pizsx69@users.noreply.github.com>
2023-10-27 22:31:29 +0200

    Update searchinrop.py

--- searchinrop.py
+++ searchinrop.py
@@ -1,12 +1,45 @@
 #usage python searchinrop.py csrop.txt "push esp" "pop" 
 import argparse
+import numpy
+ 
+
+list = []
+def Sorting(lst):
+ 
+    # list for storing the length of each string in list 
+    lenlist=[]   
+    for x in lst:
+         lenlist.append(len(x))     
+ 
+    # return a list with the index of the sorted
+    # items in the list
+    sortedindex = numpy.argsort(lenlist)  
+ 
+    # creating a dummy list where we will place the 
+    # word according to the sortedindex list 
+    lst2 = ['dummy']*len(lst)   
+ 
+    # print(sortedindex,lenlist)
+    for i in range(len(lst)):    
+ 
+        # placing element in the lst2 list by taking the
+        # value from original list lst where it should belong 
+        # in the sorted list by taking its index from sortedindex
+        lst2[i] = lst[sortedindex[i]]     
+                                         
+    return lst2
+     
+# Driver code
+
+
 
 def search_strings_in_file(file_path, search_strings):
     try:
         with open(file_path, 'r') as file:
             for line in file:
                 if all(search_string in line for search_string in search_strings):
-                    print(line, end='')
+                    list.append(line)
+                    #print(line, end='')
 
     except FileNotFoundError:
         print(f"File not found: {file_path}")
@@ -23,3 +56,7 @@ if __name__ == '__main__':
     search_strings = args.search_strings
 
     search_strings_in_file(file_path, search_strings)
+    p = Sorting(list)
+    for x in p:
+        print(x,end='')
+
